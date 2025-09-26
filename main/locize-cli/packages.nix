bdd105f034ac0855536ee35a5e0ba59f463a9c56
Chris Martin <ch.martin@gmail.com>
2025-09-26 15:02:38 -0600

    remove old version of locize-cli (#227)

--- main/locize-cli/packages.nix
+++ main/locize-cli/packages.nix
@@ -1,12 +1,9 @@
 { inputs, system, ... }:
 let
   locize-cli-packages =
-    (import ./8.0.1 { inherit inputs system; })
-    // (import ./10.3.2 { inherit inputs system; })
+    (import ./10.3.2 { inherit inputs system; })
     // (with locize-cli-packages; {
       locize-cli-default = locize-cli-10-x;
-      locize-cli-8-x = locize-cli-8-0-x;
-      locize-cli-8-0-x = locize-cli-8-0-1;
       locize-cli-10-x = locize-cli-10-3-x;
       locize-cli-10-3-x = locize-cli-10-3-2;
     });
